8b3d28e21e6ce70f867ce8e0335ae52c92818a31
Ak-it334 <aleksejkudravcev986@gmail.com>
2024-09-02 16:33:49 +0300

    Update stroka.py

--- stroka.py
+++ stroka.py
@@ -1,17 +1,1 @@
-#1st program
-print(9 ** 0.5 * 5)
-
-#2st program
-print(9.99 > 9.98 and 1000 != 1001)
-
-#3st program
-print(2 * 2 + 2)
-print(2 * (2 + 2))
-print((2 * 2 + 2) == (2 * (2 + 2)))
-
-#4st program
-bool('123.456')
-print(int(123.456 * 10))
-print(1234 % 10)
-
 
